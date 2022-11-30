4ce96ce2f0aefaf6cff9f043b1368b77aaa7a137
Emmanuel Sofuwa <emmanuelsofuwa2@gmail.com>
2022-11-30 11:36:26 +0100

    router

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import { ColorModeScript } from "@chakra-ui/react";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { App } from "./App";
@@ -7,18 +6,18 @@ import { myNewTheme } from "./components/Styles/theme";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <ColorModeScript />
-    <ChakraProvider theme={myNewTheme}>
+  <ChakraProvider theme={myNewTheme}>
+    <BrowserRouter>
       <App />
-    </ChakraProvider>
-  </React.StrictMode>
+    </BrowserRouter>
+  </ChakraProvider>
 );
 
 // If you want your app to work offline and load faster, you can change
